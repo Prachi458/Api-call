9dce8492652e9638bc5cc9e1128f8adec49e97aa
Prachi458 <pkhushal458@gmail.com>
2021-04-29 04:54:36 -0700

    API data fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      isLoading: false,
+      isError: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      isLoading: true,
+      isError: true,
+    });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          users: data,
+          isLoading: false,
+          isError: false,
+        });
+      });
+  }
+
+  renderTableHeader = () => {
+    return Object.keys(this.state.users[0]).map((attr) => (
+      <th className="row-class" key={attr}>
+        {attr.toUpperCase()}
+      </th>
+    ));
+  };
+  renderTableRows = () => {
+    return this.state.users.map((user) => {
+      return (
+        <tr key={user.id}>
+          <td className="row-class">{user.id}</td>
+          <td className="row-class">{user.name}</td>
+          <td className="row-class">{user.username}</td>
+          <td className="row-class">{user.email}</td>
+          <td className="row-class">{`${user.address.street}, ${user.address.city}, ${user.address.zipcode}`}</td>
+          <td className="row-class">{user.phone}</td>
+          <td className="row-class">{user.website}</td>
+          <td className="row-class">{`${user.company.name}, ${user.company.catchPhrase}`}</td>
+        </tr>
+      );
+    });
+  };
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+    if (this.state.isError) {
+      return <div>Error</div>;
+    }
+    return this.state.users.length > 0 ? (
+      <div>
+        <table className="table-class">
+          <thead>
+            <tr className="row-class">{this.renderTableHeader()}</tr>
+          </thead>
+          <tbody>{this.renderTableRows()}</tbody>
+        </table>
+      </div>
+    ) : (
+      <div>No users.</div>
+    );
+  }
 }
-
 export default App;
